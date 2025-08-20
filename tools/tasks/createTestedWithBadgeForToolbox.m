35b9edd3fb0bd75fcf6ec62b2167519282245930
ehennestad <ehennestad@gmail.com>
2025-08-20 22:44:27 +0200

    Delete tools/tasks/createTestedWithBadgeForToolbox.m

--- tools/tasks/createTestedWithBadgeForToolbox.m
+++
@@ -1,7 +0,0 @@
-function createTestedWithBadgeForToolbox(versionNumber)
-    arguments
-        versionNumber (1,1) string
-    end
-    projectRootDirectory = dropboxtools.projectdir();
-    matbox.tasks.createTestedWithBadgeforToolbox(versionNumber, projectRootDirectory)
-end
